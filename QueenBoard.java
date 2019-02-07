61fe11efb80fd56ee3ddbbe045d978313ce6936a
Kiran Duggirala <kduggirala10@stuy.edu>
2019-02-07 10:21:42 -0500

    Added clearBoard method, sets all indices to 0

--- QueenBoard.java
+++ QueenBoard.java
@@ -49,6 +49,12 @@ public class QueenBoard {
 	  *@throws IllegalStateException when the board starts with any non-zero value
 	  */
 	  public int countSolutions(){}
-	  
+	  private void clearBoard() {
+		  for (int i = 0; i < n; i++) {
+			  for (int j = 0; j < n; j++) {
+				  board[i][j] = 0;
+			  }
+		  }
+	  }
 
 }
