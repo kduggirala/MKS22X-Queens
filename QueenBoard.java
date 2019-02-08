8e660c5bbd71455019ebd8897c6cfb64525cb214
Kiran Duggirala <kduggirala10@stuy.edu>
2019-02-08 12:54:16 -0500

    More tests, slight modifications to implementation

--- QueenBoard.java
+++ QueenBoard.java
@@ -6,153 +6,156 @@ public class QueenBoard {
 	public QueenBoard(int size) {
 		n = size;
 		board = new int[size][size];
+		countSolutions = 0;
 	}
 	/**
-	  *@return The output string formatted as follows:
-	  *All numbers that represent queens are replaced with 'Q' 
-	  *all others are displayed as underscores '_'
-	  *There are spaces between each symbol:  
-	  *"""_ _ Q _
-	  *Q _ _ _
+	 *@return The output string formatted as follows:
+	 *All numbers that represent queens are replaced with 'Q' 
+	 *all others are displayed as underscores '_'
+	 *There are spaces between each symbol:  
+	 *"""_ _ Q _
+	 *Q _ _ _
 
-	  *_ _ _ Q
+	 *_ _ _ Q
 
-	  *_ Q _ _"""
-	  *(pythonic string notation for clarity, 
-	  *excludes the character up to the *)
-	  */
-	  public String toString(){
-		  StringBuffer boardStringBuffer = new StringBuffer();
-		  for (int[] row: board) {
-			  for (int i : row) {
-				  boardStringBuffer.append(i == -1 ? "Q" : "_");
-				  boardStringBuffer.append(" ");
-			  }
-			  boardStringBuffer.append("\n");
-		  }
-		  return boardStringBuffer.toString();
-	  }
+	 *_ Q _ _"""
+	 *(pythonic string notation for clarity, 
+	 *excludes the character up to the *)
+	 */
+	public String toString(){
+		StringBuffer boardStringBuffer = new StringBuffer();
+		for (int[] row: board) {
+			for (int i : row) {
+				boardStringBuffer.append(i == -1 ? "Q" : "_");
+				boardStringBuffer.append(" ");
+			}
+			boardStringBuffer.append("\n");
+		}
+		return boardStringBuffer.toString();
+	}
 
 
-	  
-	  /**
-	  *@return false when the board is not solveable and leaves the board filled with zeros; 
 
-	  *        true when the board is solveable, and leaves the board in a solved state
+	/**
+	 *@return false when the board is not solveable and leaves the board filled with zeros; 
 
-	  *@throws IllegalStateException when the board starts with any non-zero value
+	 *        true when the board is solveable, and leaves the board in a solved state
 
-	  */
-	  public boolean solve(){
-		  if (!isEmpty()) {
-			  throw new IllegalStateException();
-		  }
-		  boolean solvable = solveHelp(0);
-		  if (!solvable) {
-			  clearBoard();
-		  }
-		  return solvable;
-	  }
-	  
-	  private boolean solveHelp(int c) {
-		  if (c == n) {
-			  return true;
-		  }
-		  for (int i  = 0; i < n; i++) {
-			  if (board[i][c] == 0) {
-				  addQueen(i, c);
-				  if (solveHelp(c + 1)) {
-					  return true;
-				  }
-				  else {
-					  removeQueen(i, c);
-				  }
-			  }
-		  }
-		  return false;
-	  }
-	  
-	  private void addQueen(int r, int c) {
-		  board[r][c] = -1;
-		  for (int i = c + 1; i < n; i++) {
-			  for (int j = 0; j < n; j++) {
-				  //at horizontal
-				  if (j == r) {
-					  board[j][i]++;
-				  }
-				  //at diagonal
-				  if (Math.abs(j - r) == Math.abs(i - c)) {
-					  board[j][i]++;
-				  }
-			  }
-		  }
-	  }
-	  
-	  private void removeQueen(int r, int c) {
-		  if (board[r][c] != -1) {
-			  throw new IllegalArgumentException("No queen here");
-		  }
-		  board[r][c] = 0;
-		  for (int i = c + 1; i < n; i++) {
-			  for (int j = 0; j < n; j++) {
-				  //at horizontal
-				  if (j == r) {
-					  board[j][i]--;
-				  }
-				  //at diagonal
-				  if (Math.abs(j - r) == Math.abs(i - c)) {
-					  board[j][i]--;
-				  }
-			  }
-		  }
-	  }
-	  /**
-	  *@return the number of solutions found, and leaves the board filled with only 0's
-	  *@throws IllegalStateException when the board starts with any non-zero value
-	  */
-	  public int countSolutions(){
-		  if (!isEmpty()) {
-			  throw new IllegalStateException();
-		  }
-		  countSolutions = 0;
-		  countSolutionsHelp(0);
-		  clearBoard();
-		  return countSolutions;
-	  }
-	  
-	  private void countSolutionsHelp(int c) {
-		  if (c == n - 1) {
-			  for (int i  = 0; i < n; i++) {
-				  if (board[i][c] == 0) {
-					  countSolutions++;
-				  }
-			  }
-		  }
-		  else {
-			  for (int i  = 0; i < n; i++) {
-				  if (board[i][c] == 0) {
-					  addQueen(i, c);
-					  countSolutionsHelp(c+1);
-					  removeQueen(i, c);
-				  }
-			  }
-		  }
-	  }
-	  public void clearBoard() {
-		  for (int i = 0; i < n; i++) {
-			  for (int j = 0; j < n; j++) {
-				  board[i][j] = 0;
-			  }
-		  }
-	  }
-	  private boolean isEmpty() {
-		  for (int[] row: board) {
-			  for (int i : row) {
-				  if (i != 0) {
-					  return false;
-				  }
-			  }
-		  }
-		  return true;
-	  }
+	 *@throws IllegalStateException when the board starts with any non-zero value
+
+	 */
+	public boolean solve(){
+		if (!isEmpty()) {
+			throw new IllegalStateException("The board must be empty before solving");
+		}
+		boolean solvable = solveHelp(0);
+		if (!solvable) {
+			clearBoard();
+		}
+		return solvable;
+	}
+
+	private boolean solveHelp(int c) {
+		if (c == n) {
+			return true;
+		}
+		for (int i  = 0; i < n; i++) {
+			if (board[i][c] == 0) {
+				addQueen(i, c);
+				if (solveHelp(c + 1)) {
+					return true;
+				}
+				else {
+					removeQueen(i, c);
+				}
+			}
+		}
+		return false;
+	}
+
+	private void addQueen(int r, int c) {
+		board[r][c] = -1;
+		for (int i = c + 1; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				//at horizontal
+				if (j == r) {
+					board[j][i]++;
+				}
+				//at diagonal
+				if (Math.abs(j - r) == Math.abs(i - c)) {
+					board[j][i]++;
+				}
+			}
+		}
+	}
+
+	private void removeQueen(int r, int c) {
+		if (board[r][c] != -1) {
+			throw new IllegalArgumentException("No queen here");
+		}
+		board[r][c] = 0;
+		for (int i = c + 1; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				//at horizontal
+				if (j == r) {
+					board[j][i]--;
+				}
+				//at diagonal
+				if (Math.abs(j - r) == Math.abs(i - c)) {
+					board[j][i]--;
+				}
+			}
+		}
+	}
+	/**
+	 *@return the number of solutions found, and leaves the board filled with only 0's
+	 *@throws IllegalStateException when the board starts with any non-zero value
+	 */
+	public int countSolutions(){
+		if (!isEmpty()) {
+			throw new IllegalStateException("The board must be empty before solving");
+		}
+		if(countSolutions == 0) {
+			countSolutionsHelp(0);
+			clearBoard();
+		}
+		return countSolutions;
+	}
+
+	private void countSolutionsHelp(int c) {
+		if (c == n - 1) {
+			for (int i  = 0; i < n; i++) {
+				if (board[i][c] == 0) {
+					countSolutions++;
+					break;
+				}
+			}
+		}
+		else {
+			for (int i  = 0; i < n; i++) {
+				if (board[i][c] == 0) {
+					addQueen(i, c);
+					countSolutionsHelp(c+1);
+					removeQueen(i, c);
+				}
+			}
+		}
+	}
+	public void clearBoard() {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				board[i][j] = 0;
+			}
+		}
+	}
+	private boolean isEmpty() {
+		for (int[] row: board) {
+			for (int i : row) {
+				if (i != 0) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 }
--- QueenBoardTest.java
+++ QueenBoardTest.java
@@ -1,7 +1,7 @@
 
 public class QueenBoardTest {
 	public static void main(String[] args) {
-		for (int i = 4; i < 21; i++) {
+		for (int i = 0; i < 21; i++) {
 			QueenBoard q = new QueenBoard(i);
 			System.out.println(q.countSolutions());
 		}
